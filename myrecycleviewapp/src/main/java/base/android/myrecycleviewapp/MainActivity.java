0b2f7a01475f6d1754c1a8afc0fbf31a09e5503b
DevSergei <bakaikin@mail.ru>
2018-10-15 16:13:32 +0300

    myrecycleviewapp module add

---
+++ myrecycleviewapp/src/main/java/base/android/myrecycleviewapp/MainActivity.java
@@ -0,0 +1,33 @@
+package base.android.myrecycleviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        // установим аниматор по умолчанию
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        // эта установка служит для повышения производительности системы.
+        recyclerView.setHasFixedSize(true);
+
+        // будем работать со встроенным менеджером
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // установим адаптер
+        final MyAdapter adapter = new MyAdapter();
+        recyclerView.setAdapter(adapter);
+
+    }
+}
---
+++ myrecycleviewapp/src/main/java/base/android/myrecycleviewapp/MyAdapter.java
@@ -0,0 +1,41 @@
+package base.android.myrecycleviewapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+private List<Item> list = new ArrayList<>();
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item, viewGroup, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.tv_name);
+        }
+    }
+}
